8f3a41319477e545f801cb26c617c4f3c6fc9803
Carina Druce <trueblue546@gmail.com>
2022-07-20 19:37:04 +0100

    third heroku deployment

--- DB/index.js
+++ DB/index.js
@@ -1,13 +1,10 @@
 const { response } = require("express");
 const jwt_decode = require("jwt-decode");
+const connectionString = process.env.DATABASE_URL;
 
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: process.env.HEROKU_DB_USER,
-  host: process.env.HEROKU_DB_HOST,
-  database: process.env.HEROKU_DB_NAME,
-  password: process.env.HEROKU_DB_PASSWORD,
-  port: process.env.HEROKU_DB_PORT,
+  connectionString,
   ssl: {
     rejectUnauthorized: false,
   },
